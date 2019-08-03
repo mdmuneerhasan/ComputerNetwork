5d56b1b6821c6482d4d1d97ae5249916772729b4
mdmuneerhasan <md.muneerhasan@gmail.com>
2019-08-03 13:59:14 +0530

    first

---
+++ 1code1Class1.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    cout << "enter the message" ;
+    string message,encryp,decryp;
+    cin >> message;
+    cout <<"enter key";
+    int key;
+    cin >> key;
+    for(int i=0;i<message.size();i++){
+        int num=message[i]-'a'+key;
+        char c=num+'a';
+        encryp=encryp+c;
+    }
+    cout <<"encrypted message: "<< encryp<<endl;
+
+    for(int i=0;i<encryp.size();i++){
+        int num=encryp[i]-'a'-key;
+        char c=num+'a';
+        decryp=decryp+c;
+    }
+    cout <<"decrypted message: "<< decryp<<endl;
+}
---
+++ 2code2Class1.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    cout << "enter encrypted message" ;
+    string message,encryp,decryp;
+    cin >> encryp;
+    int key=1;
+    while(key++){
+        decryp="";
+    for(int i=0;i<encryp.size();i++){
+        int num=encryp[i]-'A'-key;
+        if(num<0){
+            num=num+26;
+        }
+        char c=num+'A';
+        decryp=decryp+c;
+    }
+    cout <<"key: "<<key<<" decrypted message: "<< decryp<<endl;
+    if(key>26){
+        break;
+    }
+    }
+}
